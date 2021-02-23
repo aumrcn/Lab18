ab3b3f1f09fd8e51eb128d4f49144ec8fa6d9c28
aumrcn <aumkung20728@gmail.com>
2021-02-23 17:55:51 +0700

    lab18_1.cpp completed

--- bobo.cpp
+++ bobo.cpp
@@ -6,14 +6,14 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(Rect *q, Rect *w){
+double overlap(Rect q, Rect w){
 	double L1,L2;
 	double minx , miny, maxx , maxy , LT = 0;
-    minx = min(q->x+q->w,w->x+w->w);
-    maxx = max(q->x,w->x);
+    minx = min(q.x+q.w,w.x+w.w);
+    maxx = max(q.x,w.x);
 	L1 = minx - maxx;
-	miny = min(q->y,w->y);
-    maxy = max(q->y-q->h,w->y-w->h);
+	miny = min(q.y,w.y);
+    maxy = max(q.y-q.h,w.y-w.h);
 	L2 = miny - maxy;
 	if(L1 < 0){
 		L1 = 0;
@@ -21,6 +21,7 @@ double overlap(Rect *q, Rect *w){
 	if(L2 <0){
 		L2 = 0;
 	}
+
 	LT = L1*L2;
 	return LT;
 }
@@ -28,6 +29,6 @@ double overlap(Rect *q, Rect *w){
 int main(){
 	Rect R1 = {1,1,5,5};
 Rect R2 = {7,2,3,3};
-cout << overlap(&R1,&R2);
+cout << overlap(R1,R2);
 	return 0;	
 }
--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,49 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *q, Rect *w){
+	double L1,L2;
+	double minx , miny, maxx , maxy , LT;
+	if(q->x+q->w < w->x+w->w){
+		minx = q->x+q->w;
+	}else{
+		minx = w->x+w->w;
+	}
+	if(q->x < w->x){
+		maxx = w->y;
+	}else{
+		maxx = q->y;
+	}
+	L1 = minx - maxx;
+	if(q->y < w->y){
+		miny = q->y;
+	}else{
+		miny = w->y;
+	}
+	if(q->y-q->h > w->y-w->h){
+		maxy = q->y-q->h;
+	}else{
+		maxy = w->y-w->h;
+	}
+	L2 = miny - maxy;
+	if(L1 < 0){
+		LT = 0;
+	}
+	if(L2 <0){
+		LT = 0;
+	}
+	cout << minx << " " << maxx << endl;
+	cout << miny << " " << maxy << endl;
 
+	cout << L1 << " " << L2;
+	cout << "LT before multiply = " << LT;
+	LT = L1*L2;
+	return LT;
+}
 
-
+int main(){
+	Rect R1 = {1,1,5,5};
+Rect R2 = {7,2,3,3};
+cout << overlap(&R1,&R2);
+	return 0;	
 }
